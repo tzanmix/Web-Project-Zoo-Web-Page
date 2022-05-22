145e0356cf2a56e5cc2c48adcb3a78c4375af1f2
tzanmix <102822743+tzanmix@users.noreply.github.com>
2022-05-22 18:21:56 +0300

    Add files via upload

---
+++ js/animals.js
@@ -0,0 +1,29 @@
+//Animals responsiveness
+
+const small = window.matchMedia("(max-width: 900px)");
+
+let section = document.querySelectorAll(".section");
+let sect = document.querySelectorAll(".section div")
+let img = document.querySelectorAll(".section img")
+
+console.log(img)
+console.log(sect)
+
+
+small.onchange = (small) => {
+    if (small.matches) {
+        for (let i = 0; i < img.length; i++) {
+            // section[i].insertBefore(img[i], sect[i]);
+            section[i].insertBefore(sect[i], img[i]);
+        }
+
+    }
+
+    else {
+        for (let i = 0; i < img.length; i = i + 2) {
+            // section[i].insertBefore(sect[i], img[i]);
+            section[i].insertBefore(img[i], sect[i]);
+        }
+    }
+}
+
